d51248a5cb2d3b975549f36dc2a64effdfd230f3
AlvaroCastilloCarreno <6666alvaro666@gmail.com>
2026-07-15 13:56:43 -0600

    fix: module import path for running from project root

--- lavi/main.py
+++ lavi/main.py
@@ -2,7 +2,7 @@ import sys
 import json
 import os
 
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from lavi.renderer import Renderer
 from lavi.engine.state_machine import StateMachine
